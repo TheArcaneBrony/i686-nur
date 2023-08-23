2be559609d8fa859667b002b211bbe16620a84e1
TheArcaneBrony <myrainbowdash949@gmail.com>
2023-08-23 04:12:32 +0200

    Add nheko, switch to 22.11

--- pkgs/example-package/default.nix
+++ pkgs/example-package/default.nix
@@ -1,9 +1,11 @@
-{ stdenv }:
+{ pkgs }:
 
-stdenv.mkDerivation rec {
-  name = "example-package-${version}";
-  version = "1.0";
-  src = ./.;
-  buildPhase = "echo echo Hello World > example";
-  installPhase = "install -Dm755 example $out";
+derivation {
+  system = "i686-linux";
+  name = "test";
+  builder = "/bin/sh";
+  args = [ "-c" "true > $out" ];
+  buildInputs = [
+    pkgs.nheko
+  ];
 }
